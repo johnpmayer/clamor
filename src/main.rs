352228fca032365549e18d8bb1d7fd1d6793ea0b
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2017-09-03 22:10:26 -0400

    Single system

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,36 @@ struct Velocity {
     y: f32,
 }
 
+use specs::{ReadStorage, System};
+
+struct HelloWorld;
+
+impl<'a> System<'a> for HelloWorld {
+    type SystemData = ReadStorage<'a, Position>;
+
+    fn run(&mut self, position: Self::SystemData) {
+        use specs::Join;
+
+        for position in position.join() {
+            println!("Hello, {:?}", &position);
+        }
+    }
+}
+
+use specs::World;
+
+use specs::RunNow;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Start!");
+
+    let mut world = World::new();
+    world.register::<Position>();
+
+    let ball = world.create_entity().with(Position { x: 4.0, y: 7.0 }).build();
+
+    let mut hello_world = HelloWorld;
+    hello_world.run_now(&world.res);
+
+    println!("End!");    
 }
