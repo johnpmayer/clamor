2568b5cdf474d7195b3c308adca0a457b53a0917
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2017-10-27 13:35:44 -0400

    Node adjacency, beginning to look at subdivision

--- src/geo.rs
+++ src/geo.rs
@@ -8,7 +8,7 @@ use std::f32::{self, consts};
 use nalgebra::geometry::Rotation3;
 use nalgebra::core::Vector3;
 
-type NetCoordinate = (usize, usize);
+type NetCoordinate = (i32, i32);
 
 #[derive(Debug)]
 struct NetNode {
@@ -19,6 +19,12 @@ struct NetNode {
 #[derive(Debug)]
 struct Net {
     nodes: HashMap<NetCoordinate, Rc<NetNode>>,
+    adjacency: HashMap<NetCoordinate, Vec<NetCoordinate>>,
+}
+
+#[derive(Debug)]
+enum NetError {
+    InvalidCoordinate
 }
 
 impl Net {
@@ -110,8 +116,87 @@ impl Net {
             println!("{:?}", node)
         }
 
-        Net { nodes }
+        let mut adjacency: HashMap<NetCoordinate, Vec<NetCoordinate>> = HashMap::new();
 
+        for node_coordinate in nodes.keys() {
+
+            if nodes.get(node_coordinate).unwrap().coordinates[0] != *node_coordinate {
+                // Only store canonical coordinates in the adjacency lookup
+                continue
+            }
+
+            let neighbors = Net::canonical_neighbors(&nodes, node_coordinate).unwrap();
+            println!("{:?}, {:?}", node_coordinate, neighbors);
+            assert!(neighbors.len() == 5);
+            adjacency.insert(*node_coordinate, neighbors);
+        }
+
+        Net { nodes, adjacency }
+
+    }
+
+    fn face_base_coordinates() -> Vec<NetCoordinate> {
+        let mut coordinates = Vec::new();
+        for i in 0..5 {
+            coordinates.push((i, i));
+            coordinates.push((i + 1, i));
+        }
+        coordinates
+    }
+
+    fn canonical_neighbors(nodes: &HashMap<NetCoordinate, Rc<NetNode>>, coordinate: &NetCoordinate) -> Result<Vec<NetCoordinate>, NetError> {
+        let mut neighbors = HashSet::new();
+        let mut counter_clockwise_neighbors = Vec::new();
+
+        let offsets: [NetCoordinate; 6] = [
+            (1,0),(1,1),(0,1),
+            (-1,0),(-1,-1),(0,-1),
+        ];
+
+        let node: &Rc<NetNode> = nodes.get(&coordinate).ok_or(NetError::InvalidCoordinate)?;
+
+        for &node_coordinate in node.coordinates.iter() {
+            let (node_x, node_y) = node_coordinate;
+            for offset in offsets.iter() {
+                let (offset_x, offset_y) = *offset;
+
+                let test_coordinate = (node_x + offset_x, node_y + offset_y);
+                if node.coordinates.iter().find(|c| **c == test_coordinate) ==  None {
+                    for neighbor_node in nodes.get(&test_coordinate) {
+                        // Only insert the canonical coordinate
+                        let canonical_neighbor_coordinate = neighbor_node.coordinates[0];
+                        if neighbors.insert(canonical_neighbor_coordinate) {
+                           counter_clockwise_neighbors.push(canonical_neighbor_coordinate)
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(counter_clockwise_neighbors)
+    }
+
+    fn subdivide(&self) -> Net {
+
+        /*
+        Scale the 12 primary nodes and their aliases
+        Create canonical and alias coordinates for all non-canonical edge nodes
+            North pole x5
+                For each north hemisphere non-polar primary node:
+                    the canonical edge is defined as: node + (0,1) = north pole
+                    the non-canonical edge is defined as: node + (-1, 0) = north pole
+            South pole x5
+                For each south hemisphere non-polar primary node:
+                    the canonical edge is defined as: node + (1,0) = south pole
+                    the non-canonical edge is defined as: node + (0, -1) = south pole
+            Tropics belt x1
+                The canonical edge is defined as: (0,0) <--> (1,0)
+                The non-canonical edge is defined as: (5,5) <--> (6,5)
+        Create all remaining 'edge' nodes, relative to the 10 non-polar primary nodes
+        Create all remaining internal nodes, relative to the the 10 non-polar primary nodes, (2x triangles, top & bottom)
+        */
+
+        panic!("TODO")
     }
 }
 
